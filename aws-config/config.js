71e67de3789f3610e7303e250f21c68cbc75ce12
Justin Licari <justinlicari@Justins-MacBook-Air.local>
2021-05-24 19:40:53 -0400

    Note about keys for AWS

--- aws-config/config.js
+++ aws-config/config.js
@@ -4,8 +4,8 @@ module.exports = {
       //Provide details for local configuration
     },
     aws_remote_config: {
-      accessKeyId: 'AKIAT3S2S7XRDLBJI5PL',
-      secretAccessKey: 'tFqt8S94KAFPgN9mspZ0ZHASnoIRNnkQV7C4BGqb',
+      accessKeyId: 'AKIAT3S2S7XRDLBJI5PL', // replace with a valid IAM key
+      secretAccessKey: 'tFqt8S94KAFPgN9mspZ0ZHASnoIRNnkQV7C4BGqb', // replace witha  valid secret key
       region: 'ca-central-1',
     }
 };
